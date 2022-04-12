74389ba5fa451daad0a8abfdd3c4b5359ef0f5e6
Ryan Chow <ryanchowdev@gmail.com>
2022-04-12 14:42:48 -0700

    Using .env file for bot token

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+import os
 import discord
 from discord.ext import commands
+from dotenv import load_dotenv
+
+# get token from .env file
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
 
 #variables
 l_names = [] #list of names
 d_names = {} #dictionary of names
-token = ''
 bot = commands.Bot(command_prefix='#')
 
 @bot.command()
@@ -14,7 +19,4 @@ async def hello(ctx):
 #@bot.command()
 #async def schedule_daily_message():
 
-
-#insert token into bot.run( discord bot token )
-
-bot.run(token)
+bot.run(TOKEN)
