65ebc8933da03d9bfeb0185861d14f9ec9c9e58a
Randy Kim <rkim28@ucsc.edu>
2022-04-14 00:11:49 -0700

    Added basic embedded help message
    Currently involves hardcoding in commands. Basic template.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from re import A
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -10,13 +11,51 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 #variables
 l_names = [] #list of names
 d_names = {} #dictionary of names
-bot = commands.Bot(command_prefix='#')
+bot = commands.Bot(command_prefix='=')
 
 @bot.command()
 async def hello(ctx):
     await ctx.reply("Hello!")
 
-#@bot.command()
-#async def schedule_daily_message():
+@bot.command()
+async def helper(ctx):
+   embed=discord.Embed(
+      title="RoommateHelper : Help Menu ( = )", 
+      description="",
+      color=discord.Color.blue()
+      )
+   
+   #embed.set_author(name="RoommateHelper : Help Menu")
+   embed.add_field(
+      name="**command1**", 
+      value="Show description of command 1.", 
+      inline=False
+      )
+   
+   embed.add_field(
+      name="**command2**", 
+      value="Show description of command 2.", 
+      inline=False
+      )
+   
+   embed.add_field(
+      name="**command3**", 
+      value="Show description of command 3.", 
+      inline=False
+      )
+   
+   embed.add_field(
+      name="**command4**", 
+      value="Show description of command 4.", 
+      inline=False
+      )
+   
+   embed.add_field(
+      name="**command5**", 
+      value="Show description of command 5.", 
+      inline=False
+      )
+   
+   await ctx.send(embed=embed)
 
 bot.run(TOKEN)
