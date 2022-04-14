f1dc69b923b147b0abca1134e4a67d4eef647f33
Timothy Ding <tding215@gmail.com>
2022-04-14 12:07:57 -0700

    Moved scheduling to another file
    Title. Forgot to add main.py with previous commit

--- main.py
+++ main.py
@@ -10,8 +10,6 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 #variables
-l_names = [] #list of names
-d_names = {} #dictionary of names
 bot = commands.Bot(command_prefix='=')
 
 builtins.bot = bot
@@ -20,37 +18,9 @@ builtins.bot = bot
 async def hello(ctx):
     await ctx.reply("Hello!")
 
-@bot.command()
-async def schedule(ctx, paramOne:str = "m" ,paramTwo:int = 1,message:str = "MSG"):
-    if type(paramTwo) != int:
-         await ctx.reply("Unrecognized time frame")
-
-    time = paramTwo
-    if paramOne in ["m","minute","min"]:
-        paramOne = "minute"
-    elif paramOne in ["h","hour"]:
-        paramOne = "hour"
-        time*=60
-    elif paramOne in ["d","day"]:
-        paramOne = "day"
-        time*=60*24
-    else:
-        await ctx.reply("Unrecognized please use m for minutes, h for hours, d for days instead of " + paramOne)
-    await ctx.reply("Scheduled a message every "+ str(paramTwo)+" "+paramOne)
-    scheduledMessage.change_interval(minutes = int(time))
-    scheduledMessage.start(ctx,message)
-
-@bot.command()
-async def stop(ctx):
-    scheduledMessage.cancel()
-    await ctx.reply("Stopped message")
-
-@tasks.loop(minutes=1)
-async def scheduledMessage(ctx,message):
-    await ctx.send(message)
-
 # import bot features
 import weather
 import money
+import schedule
 
 bot.run(TOKEN)
