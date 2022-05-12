269795c5a0bace3923fd4471ded99779fe3184af
Randy Kim <rkim28@ucsc.edu>
2022-05-12 16:59:04 -0700

    Updated coin flip with image

--- coin.py
+++ coin.py
@@ -7,8 +7,26 @@ from builtins import bot
 @bot.command()
 async def flip(ctx):
    res = random.randint(0,1)
-   if res == 0:
-       await ctx.reply("Heads")
-   else:
-      await ctx.reply("Tails")
    
+   embed=discord.Embed(
+         title="RoommateHelper : Coin Flip", 
+         description="",
+         color=discord.Color.blue()
+      )
+   
+   if res == 0:
+      embed.add_field(
+         name="You got:", 
+         value="Heads", 
+         inline=False
+      )
+      embed.set_image(url="https://i.imgur.com/DsLFwRO.png")
+   else:
+      embed.add_field(
+         name="You got:", 
+         value="Tails", 
+         inline=False
+      )
+      embed.set_image(url="https://i.imgur.com/3Xqr7Eh.png")
+   
+   await ctx.send(embed=embed)
