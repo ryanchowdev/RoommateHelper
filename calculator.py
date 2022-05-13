835b35c31a8cd8577dec7e2056e713ac4b76459d
Randy Kim <rkim28@ucsc.edu>
2022-05-13 14:50:29 -0700

    Removing unnecessary commands from calculator

--- calculator.py
+++ calculator.py
@@ -30,33 +30,3 @@ async def calc(ctx,*,equation=""):
             await ctx.send("Error: Invalid Input for Equation")
       else:
          await ctx.send("Error: Invalid Input for Equation")
-   
-@bot.command()
-async def add(ctx, x: float, y: float):
-   res = x + y
-   await ctx.send(res)
-   
-@bot.command()
-async def sub(ctx, x: float, y: float):
-   res = x - y
-   await ctx.send(res)
-   
-@bot.command()
-async def mul(ctx, x: float, y: float):
-   res = x * y
-   await ctx.send(res)
-   
-@bot.command()
-async def div(ctx, x: float, y: float):
-   res = x / y
-   await ctx.send(res)
-   
-@bot.command()
-async def sqrt(ctx, x: float):
-   res = math.sqrt(x)
-   await ctx.send(res)
-   
-@bot.command()
-async def rand(ctx, x: int, y: int):
-   res = random.randint(x,y)
-   await ctx.send(res)
