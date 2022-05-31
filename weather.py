2a75c03ed8bebc6ac4c2a2276e0e21964a00ad5a
Ferdinand Adell <fadell@ucsc.edu>
2022-05-31 10:28:55 -0700

    Refactored weather.py to make unit testing easier

--- weather.py
+++ weather.py
@@ -1,42 +1,7 @@
-import os
-import aiosqlite
-import urllib3
-import json
 from builtins import bot
-
-#sqlite
-DBFILE = "main.db"
-
-# https://openweathermap.org/api - Free weather API
+import weatherFunctions
+import os
 OPENWEATHER_KEY = os.getenv('OPENWEATHER_KEY')
-
-units = {"imperial": {"temp": "F", "wind": "mi/h"},
-         "metric": {"temp": "C", "wind": "m/s"}}
-
-
-def getWeather(city, unit_sys):
-    """returns a string describing the current weather in the specified city."""
-    WEATHER_API = "https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s&units=%s" % (
-        city, OPENWEATHER_KEY, unit_sys)
-    http = urllib3.PoolManager()
-    r = http.request('GET', WEATHER_API)
-    if r.status != 200:
-        return "Error retrieving weather data: HTTP error %s" % r.status
-    data = json.loads(r.data.decode('utf-8'))
-    temp_unit = units[unit_sys]['temp']
-    wind_unit = units[unit_sys]['wind']
-    city = data['name']
-    conditions = data['weather'][0]['description']
-    temp = data['main']['temp']
-    temp_max = data['main']['temp_max']
-    temp_min = data['main']['temp_min']
-    feels_like = data['main']['feels_like']
-    humidity = data['main']['humidity']
-    wind = data['wind']['speed']
-    weather_response = "In ***%s***, it's **%s °%s** out, but it **feels like %s °%s**.\n*Temperatures will range from %s °%s - %s °%s.*\n*Humidity is at %s%% and wind is blowing at %s %s*.\nCurrent conditions: **%s**" % (
-        city, temp, temp_unit, feels_like, temp_unit, temp_min, temp_unit, temp_max, temp_unit, humidity, wind, wind_unit, conditions)
-    return weather_response
-
 
 @bot.command()
 async def setCity(ctx):
@@ -45,21 +10,11 @@ async def setCity(ctx):
     """
     cmd = ctx.message.content.split(" ")
     if len(cmd) == 1:
-        await ctx.reply("Usage:\n`=setCity <city>`")
+        return "Usage:\n`=setCity <city>`"
     else:
         city = " ".join(cmd[1:])
-        # update sqlite db
-        async with aiosqlite.connect("main.db") as db:
-            async with db.cursor() as cursor:
-                await cursor.execute("SELECT * FROM localeTable WHERE guild = ?", (ctx.guild.id,))
-                data = await cursor.fetchone()
-                if data:
-                    await cursor.execute("UPDATE localeTable SET city = ? WHERE guild = ?", (city, ctx.guild.id))
-                else:
-                    await cursor.execute("INSERT INTO localeTable (guild,city) VALUES (?, ?)", (ctx.guild.id, city))
-                await db.commit()
-        await ctx.reply(f"Location set to {city}")
-
+    msg = await weatherFunctions.setCity(city, ctx.guild.id)
+    await ctx.reply(msg)
 
 @bot.command()
 async def setUnits(ctx):
@@ -78,18 +33,8 @@ async def setUnits(ctx):
     else:
         await ctx.reply("Usage:\n`=setUnits (i)mperial OR (m)etric`")
         return
-
-    # update sqlite db
-    async with aiosqlite.connect("main.db") as db:
-        async with db.cursor() as cursor:
-            await cursor.execute("SELECT * FROM localeTable WHERE guild = ?", (ctx.guild.id,))
-            data = await cursor.fetchone()
-            if data:
-                await cursor.execute("UPDATE localeTable SET unit_sys = ? WHERE guild = ?", (unit_sys, ctx.guild.id))
-            else:
-                await cursor.execute("INSERT INTO localeTable (guild,unit_sys) VALUES (?, ?)", (ctx.guild.id, unit_sys))
-            await db.commit()
-    await ctx.reply(f"Units set to {unit_sys}")
+    msg = await weatherFunctions.setUnits(unit_sys, ctx.guild.id)
+    await ctx.reply(msg)
 
 
 @bot.command()
@@ -97,15 +42,5 @@ async def weather(ctx):
     """
     replies to the user with the current weather in their city
     """
-    async with aiosqlite.connect(DBFILE) as db:
-        async with db.cursor() as cursor:
-            await cursor.execute("SELECT * FROM localeTable WHERE guild = ?",(ctx.guild.id,))
-            await db.commit()
-            data = await cursor.fetchone()
-            if data:
-                city = data[1]
-                # get unit system from db, default to imperial
-                unit_sys = data[2] if data[2] else 'imperial'
-                await ctx.reply(getWeather(city, unit_sys))
-            else:
-                await ctx.reply("Please set your city first with `=setCity <city>`")
+    msg = await weatherFunctions.weather(ctx.guild.id, OPENWEATHER_KEY)
+    await ctx.reply(msg)
